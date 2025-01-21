b08ab3ed023b310f981adcfef8ef9db34a5f4700
nazim uddin <mdnajim1247@gmail.com>
2025-01-21 08:31:56 +0600

    my added property section designed

--- src/pages/dashboard/pages/AddedProperties.jsx
+++ src/pages/dashboard/pages/AddedProperties.jsx
@@ -2,6 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../provider/AuthProvider";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { IoLocationSharp } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 
 const AddedProperties = () => {
@@ -38,7 +40,35 @@ const AddedProperties = () => {
     return (
         <div>
             properties: {properties.length}
-            
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {
+                properties.map(property => 
+                    <div key={property._id} className="card shadow h-full flex flex-col justify-between">
+                    <figure className="p-4">
+                      <img
+                        src={property.image}
+                        alt=""
+                        className="rounded-xl" />
+                    </figure>
+                    <div className="p-4 space-y-2">
+                      <h2 className="card-title">{property.title}</h2>
+                      <div className="flex items-center justify-between">
+                      <img className="w-12 h-12 rounded-full" src={property.agentImage} alt="" />
+                      <h3 className="text-md font-semibold">{property.agentName}</h3>
+                      </div>
+                      <div className="flex items-center justify-between">
+                      <p className="flex items-center"><IoLocationSharp />{property.location}</p>
+                      <p>${property.price}</p>
+                      </div>
+                      <div className="flex flex-col md:flex-row gap-2">
+                        <Link to={`/dashboard/updateProperty/${property._id}`}><button className="btn btn-sm btn-primary w-full">Update</button></Link>
+                        <button className="btn btn-sm btn-primary">Delete</button>
+                      </div>
+                    </div>
+                  </div>        
+                )
+            }
+            </div>
         </div>
     );
 };
