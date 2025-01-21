04e5ccc93fd017533e6a309432b45b1593428d31
nazim uddin <mdnajim1247@gmail.com>
2025-01-21 20:31:12 +0600

    my reviews delete functionality done

--- src/pages/dashboard/pages/MyReviews.jsx
+++ src/pages/dashboard/pages/MyReviews.jsx
@@ -1,19 +1,56 @@
 import { useContext, useEffect, useState } from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { AuthContext } from "../../../provider/AuthProvider";
+import Swal from "sweetalert2";
+import { useQuery } from "@tanstack/react-query";
 
 const MyReviews = () => {
     const {user} = useContext(AuthContext);
-    const [reviews, setReviews] = useState([]);
     const axiosSecure = useAxiosSecure();
+    // const [reviews, setReviews] = useState([]);
 
-    useEffect(()=>{
-        const fetchAllReviews = async ()=>{
+    // useEffect(()=>{
+    //     const fetchAllReviews = async ()=>{
+    //         const {data} = await axiosSecure.get(`/reviews/${user?.email}`)
+    //         return setReviews(data);
+    //     }
+    //     fetchAllReviews();
+    // },[])
+
+    const {data: reviews = [], isLoading, refetch} = useQuery({
+        queryKey: ['reviews', user?.email],
+        queryFn: async()=>{
             const {data} = await axiosSecure.get(`/reviews/${user?.email}`)
-            return setReviews(data);
+            return data;
         }
-        fetchAllReviews();
-    },[])
+    })
+    if(isLoading) return <p className="text-center"><span className="loading loading-spinner loading-lg"></span></p>
+
+    const handleReview = (id)=>{
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then(async(result) => {
+            if (result.isConfirmed) {
+                await axiosSecure.delete(`/reviews/${id}`)
+                .then(res =>{
+                    if(res.data.deletedCount > 0){
+                        refetch()
+                        Swal.fire({
+                          title: "Deleted!",
+                          text: "Your file has been deleted.",
+                          icon: "success"
+                        });
+                    }
+                })
+            }
+          });
+    }
 
     return (
         <div>
@@ -25,7 +62,7 @@ const MyReviews = () => {
                         <h2>Agent: {review.property.agentName}</h2>
                         <p>Date: {review.date}</p>
                         <p>Description: {review.rev}</p>
-                        <button className="btn btn-sm btn-primary">Delete</button>
+                        <button onClick={()=> handleReview(review._id)} className="btn btn-sm btn-primary">Delete</button>
                     </div>
                 )}
             </div>
